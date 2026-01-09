822525d56109ef96e007419b6a51bf15be76a7ed
MuntahaZaigham1 <muntahazaigham10@gmail.com>
2026-01-09 15:06:33 +0500

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('fc-angular-material-theme-palette-generator');
+  protected readonly title = signal('fc-angular-material-theme-palette-generator'); 
 }
